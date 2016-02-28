9d4cfe64ec229067903b51dc7492fb3c7cf6831a
Jaya Wijono <jayzzwijono@yahoo.com>
2016-02-28 16:36:24 +1100

    fix bugs to view submissions in admin and app show page

--- app/dashboards/submission_dashboard.rb
+++ app/dashboards/submission_dashboard.rb
@@ -54,6 +54,7 @@ class SubmissionDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :code_test,
+    :status,
     :name,
     :email,
     :role,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :submissions, only: [:show]
   namespace :admin do
+    resources :submissions
     resources :users
     resources :code_tests
     resources :criteria
